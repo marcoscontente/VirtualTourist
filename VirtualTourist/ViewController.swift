ab7428ceb06a4b457bc77def8f43ceea1e06a3c9
Marcos Contente <stoneyourmarcos@gmail.com>
2019-02-25 22:54:02 -0300

    Create project

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Marcos Vinicius Goncalves Contente on 25/02/19.
+//  Copyright © 2019 Marcos Vinicius Goncalves Contente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
